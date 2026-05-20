34e784c857b2a9adbfb343ade7427d4624435856
Jyoti180393 <pokhariajyoti18@gmail.com>
2026-05-20 13:22:58 +0530

    Updating cod and readMe

--- JS-TS/Counting-Bit.js
+++ JS-TS/Counting-Bit.js
@@ -3,8 +3,10 @@ document.getElementById("title").innerHTML = "Counting Bits";
 function countBits(num) {
   const res = new Uint8Array(num + 1);
   for (let i = 0; i < res.length; i++) {
-    res[i] = res[Math.floor(i / 2)] + (i % 2);
-    // res[i] = res[i >> 1] + (i & 1);
+    // res[i] = res[Math.floor(i / 2)] + (i % 2);
+    // can be written like this, using division and modulus operators
+    res[i] = res[i >> 1] + (i & 1);
+    // can also be written like this, using bitwise operators for better performance
   }
   return [...res];
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Learning and Understanding the problem and solutions
+
+Problems solved
+
+- Counting Bits
+- TwoSum
